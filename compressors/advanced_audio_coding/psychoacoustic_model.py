4d84094383a22c21aee47602c999fcd01baffaa3
oliver-johnson1 <91568792+oliver-johnson1@users.noreply.github.com>
2022-12-02 09:14:38 -0800

    Fixed bugs

--- compressors/advanced_audio_coding/psychoacoustic_model.py
+++ compressors/advanced_audio_coding/psychoacoustic_model.py
@@ -21,7 +21,7 @@ def calculateThresholds(X, s_h, s_l, thr_q_prev,thr_quiet):
     # Calculation of the energy spectrum
     scalefactor, N = get_scalefactor_bands()
     en = np.zeros(N)
-    for n in N:
+    for n in range(N):
         X_n = X[scalefactor[n]:scalefactor[n+1]]
         en[n] = np.sum(X_n*X_n)
 
@@ -34,22 +34,56 @@ def calculateThresholds(X, s_h, s_l, thr_q_prev,thr_quiet):
     # spreading
     thr_spr_prime = np.zeros(N)
     
-    for n in N:
-        thr_spr_prime[n] = max(thr_scaled[n],s_h[n]*thr_scaled[n-1])
+    for n in range(N):
+        if n == 0:
+            thr_spr_prime[n] = thr_scaled[n]
+        else:
+            thr_spr_prime[n] = max(thr_scaled[n],s_h[n]*thr_scaled[n-1])
     thr_spr = np.zeros(N)
-    for n in N:
-        thr_spr[n] = max(thr_spr_prime[n],s_l[n]*thr_spr_prime[n+1])
+    for n in range(N):
+        if n == N-1:
+            thr_spr[n] = thr_spr_prime[n]
+        else:
+            thr_spr[n] = max(thr_spr_prime[n],s_l[n]*thr_spr_prime[n+1])
 
     # threshold in quiet
     thr_q = np.zeros(N)
-    for n in N:
+    """
+    for n in range(N):
         thr_q[n] = max(thr_spr[n],thr_quiet[n])
-
+    """
+    thr_q = thr_spr
     # pre-echo control
     rpelev = 2
     rpmin = 0.01
     thr = np.zeros(N)
-    for n in N:
+    for n in range(N):
         thr[n] = max(rpmin*thr_q[n],min(thr_q[n],rpelev*thr_q_prev) )
     
     return thr, thr_q
+
+
+def calculateSpreadedEnergy(bitrate=44100):
+    """
+    Spreaded Energy Calculation as per section 5.4.3
+    Only consider long blocks
+    """
+
+    scalefactor, N = get_scalefactor_bands()
+    scalefactor_bark = freq_to_bark(scalefactor)
+    delta_bark = np.diff(scalefactor_bark)
+    #
+    s_l = 30 * delta_bark
+    if bitrate > 22000:
+        s_h = 100 * delta_bark # or 10**(20/10)=100
+    else:
+        s_h = 31.62 * delta_bark # or 10**(15/10)=31.62
+    return s_l, s_h
+
+def freq_to_bark(freq):
+    return 13 * np.arctan(0.00076 * freq) + 3.5 * np.arctan((freq/7500)**2)
+
+
+if __name__ == "__main__":
+    s_l, s_h = calculateSpreadedEnergy()
+    print(calculateThresholds(1000*np.ones([1024]),s_h,s_l,0,0))
--- compressors/advanced_audio_coding/scalefactor_bands.py
+++ compressors/advanced_audio_coding/scalefactor_bands.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def get_scalefactor_bands():
     """
     swb_offset_long_window[swb]
@@ -16,10 +18,10 @@ def get_scalefactor_bands():
     # LONG_WINDOW, LONG_START_WINDOW, LONG_STOP_WINDOW at 44.1 kHz and 48 kHz
     # Our sample wave file has a sample rate of 44100 Hz
     num_swb_long_window = 49
-    swb_offset_long_window = [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 48, 56, 64, 72, 80, 88, 
-                              96, 108, 120, 132, 144, 160, 176, 196, 216, 240, 264, 292, 320, 352, 
-                              384, 416, 448, 480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800, 
-                              832, 864, 896, 928, 1024]
+    swb_offset_long_window = np.array([0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 48, 56, 64, 72, 80, 88, 
+                                       96, 108, 120, 132, 144, 160, 176, 196, 216, 240, 264, 292, 320, 352, 
+                                       384, 416, 448, 480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800, 
+                                       832, 864, 896, 928, 1024])
     return swb_offset_long_window, num_swb_long_window
 
 
